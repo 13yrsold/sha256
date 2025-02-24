0c30852b04065698b483609038176f324feda4fd
Ridge Taylor <113702161+13yrsold@users.noreply.github.com>
2025-02-24 08:25:16 -0500

    Create test.java

---
+++ test.java
@@ -0,0 +1,21 @@
+// Arrays
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> network = new ArrayList<>();
+        network.add("[ERROR]");
+        network.add("[WARN]");
+        network.add("[MAWBORN.XML]");
+
+        for (int index = 0; index < network.size(); index++)
+            sout(network.get(index));
+        }
+    }
+
+    public static void sout(String message) {
+        System.out.println(message);
+    }
+}
