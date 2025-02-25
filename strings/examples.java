e64a26102c04965c6ee97d693308431d92d8bd1d
Ridge Taylor <113702161+13yrsold@users.noreply.github.com>
2025-02-25 09:03:40 -0500

    Create examples.java

---
+++ strings/examples.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String sub = "Bruh";
+        System.out.println(sub.substring(1, 4)); // Prints out "ruh"
+
+        String length = "BRURHLDJFIEJ";
+        System.out.println(length.length()); // Prints out 12
+
+        String hash = "MYPASSWORD";
+        System.out.println(hash.hashCode());
+
+        String toupper = "bruhwhat";
+        System.out.println(toupper.toUpperCase());
+    }
+}
