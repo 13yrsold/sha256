e76f4942df008c8a5a0c11a79f833cc258752624
Ridge Taylor <113702161+13yrsold@users.noreply.github.com>
2025-02-26 09:52:32 -0500

    Create moreclass.java

---
+++ moreclass.java
@@ -0,0 +1,12 @@
+public class Users {
+    public int UserId() {
+        return 503;
+    }
+
+    public static void main(String[] args) {
+        Users mawborn = new Users();
+        int userId = mawborn.UserId();
+
+        System.out.println("UserId: " + userId);
+    }
+}
